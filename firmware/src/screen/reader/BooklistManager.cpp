5bc7ece94cb196d774b539673103c139910a3350
Kolodieiev <valerij.kolodeev@gmail.com>
2024-06-28 11:58:42 +0300

    Update BooklistManager.cpp

--- firmware/src/screen/reader/BooklistManager.cpp
+++ firmware/src/screen/reader/BooklistManager.cpp
@@ -100,16 +100,40 @@ size_t BooklistManager::getBookSize(const char *book_dir_name, const char *book_
     return f_size;
 }
 
-String BooklistManager::readText(const char *book_dir_name, const char *book_name, uint64_t pos, uint64_t size)
+bool BooklistManager::containCyrillic(const char *book_dir_name, const char *book_name)
 {
-    char *buffer = new char[size + 1];
+    String path = getBookPath(book_dir_name, book_name);
 
-    if (!buffer)
+    if (path.isEmpty())
     {
-        log_e("Помилка створення буферу на %lu байт", size);
-        return "";
+        log_e("Файл не знайдено: %s %s", book_dir_name, book_name);
+        return false;
     }
 
+    File f = SD.open(path.c_str(), FILE_READ);
+
+    if (!f)
+        return false;
+
+    String str = f.readString();
+
+    for (size_t i{0}; i < str.length(); ++i)
+    {
+        if (isCyrillic(str.c_str()[i]))
+            return true;
+    }
+
+    return false;
+}
+
+bool BooklistManager::isCyrillic(char ch)
+{
+    unsigned char uc = static_cast<unsigned char>(ch);
+    return (uc >= 0xC0 && uc <= 0xFF) || (uc >= 0x80 && uc <= 0xBF);
+}
+
+String BooklistManager::readText(bool &out_is_eof, const char *book_dir_name, const char *book_name, uint64_t pos, uint64_t size)
+{
     String book_path = getBookPath(book_dir_name, book_name);
 
     if (book_path.isEmpty())
@@ -126,18 +150,65 @@ String BooklistManager::readText(const char *book_dir_name, const char *book_nam
         return "";
     }
 
+    String ret_str;
+
+    char *buffer = new char[size + 1];
+    if (!buffer)
+    {
+        log_e("Помилка створення буферу на %lu байт", size);
+        goto exit;
+    }
+
     if (!file.seek(pos))
     {
         log_e("Помилка встановлення позиції: %lu", pos);
+        goto exit;
     }
 
-    size_t bytes_read = file.readBytes(buffer, size);
-    buffer[bytes_read] = '\0';
+    {
+        size_t bytes_read = file.readBytes(buffer, size);
 
-    String str = String(buffer);
+        if (bytes_read == size)
+            out_is_eof = false;
+        else
+            out_is_eof = true;
 
+        buffer[bytes_read] = '\0';
+
+        if (bytes_read > 2 && (buffer[bytes_read - 1] & 0x80) != 0)
+        {
+            size_t correct_char_pos{0};
+
+            for (size_t i{bytes_read - 2}; i > 0; --i)
+            {
+                if ((buffer[i] & 0x80) == 0)
+                {
+                    correct_char_pos = i;
+                    break;
+                }
+            }
+
+            if (correct_char_pos > 0)
+            {
+                char *temp = new char[correct_char_pos + 2];
+                if (temp)
+                {
+                    memcpy(temp, buffer, correct_char_pos + 1);
+                    temp[correct_char_pos + 1] = '\0';
+
+                    ret_str = String(temp);
+                    delete[] temp;
+                }
+                else
+                    log_e("Помилка створення temp буферу на %lu байт", size);
+            }
+        }
+        else
+            ret_str = String(buffer);
+    }
+
+exit:
     file.close();
     delete[] buffer;
-
-    return str;
+    return ret_str;
 }
