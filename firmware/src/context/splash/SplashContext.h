7851980c43fba96939a31d7f7faf5fc343a22f20
Kolodieiev <valerij.kolodeev@gmail.com>
2025-01-11 20:42:16 +0200

    Зменшено час запуску

--- firmware/src/context/splash/SplashContext.h
+++ firmware/src/context/splash/SplashContext.h
@@ -15,7 +15,7 @@ protected:
     virtual void update() override;
     //
 private:
-    const uint16_t SHOWING_INIT_TIME{2000};
+    const uint16_t SHOWING_INIT_TIME{1000};
 
     unsigned long _start_time;
 
